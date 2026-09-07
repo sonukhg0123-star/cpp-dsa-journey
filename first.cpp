3ba59aa25415ea6f7bbeca320147c7c18daa7dc9
sonu kumar <sonukhg0123@gmail.com>
2026-09-08 01:04:24 +0530

    Add files via upload
    this is my first class as a beginner and i have done with my first class many basic things.

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"hello sonu"<<endl;
+  cout<<"hello sonika\n";
+  cout<<"hello sonu\n";
+  cout<<45<<endl;
+  cout<<"45\n";
+  cout<<45+1<<endl;
+  cout<<"45+1\n";
+  int x;
+  x=8;
+  cout<<x+2;
+  cout<<"\nx";
+}
