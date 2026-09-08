6045ab92fb95d8c808da01e272d829e863ab27a3
sonu kumar <sonukhg0123@gmail.com>
2026-09-08 23:35:07 +0530

    Add files via upload
    this class is covered variable topics like int, float.

---
+++ 2ndclass_basic.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 7;
+    int b = 3;
+    a = b;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    int x = 7;
+    x += 8;
+    cout<<x<<endl;
+    x -= 8;
+    cout<<x<<endl;
+    x *= 8;
+    cout<<x<<endl;
+    x /= 8;
+    cout<<x<<endl;
+    float c = 6;
+    c = c + 7;
+    cout<<c<<endl;
+    c = c - 7;
+    cout<<c<<endl;
+    c = c * 7;
+    cout<<c<<endl;
+    c = c / 8;
+    cout<<c<<endl;
+    
+}
