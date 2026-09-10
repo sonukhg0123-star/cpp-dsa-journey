9c4158725a0db2d2cdedcde3d98b3adb0ee96116
sonu kumar <sonukhg0123@gmail.com>
2026-09-11 00:52:04 +0530

    Add files via upload

---
+++ Booldatatype.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool x = true; // output should be 1;
+    cout<<x<<endl;
+    bool y = false; // output should be 0;
+    cout<<y<<endl;
+//relation opreators
+    cout<<(5<6)<<endl;
+    cout<<(5>6)<<endl;
+    cout<<(5==6)<<endl;
+    cout<<(5==5)<<endl;
+    cout<<(5!=5)<<endl;
+    cout<<(5!=4)<<endl;
+    
+}
