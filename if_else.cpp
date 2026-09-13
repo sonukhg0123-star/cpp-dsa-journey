834e2f80fc6f8ab068c90e22e800e3c2910fb71c
sonu kumar <sonukhg0123@gmail.com>
2026-09-13 23:29:26 +0530

    Restore and clean up if_else.cpp code

--- if_else.cpp
+++ if_else.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int main(){/*
+int main(){
     // take age as input and tell if person can drive or not
     int age;
     cout<<"enter your age:";
@@ -8,6 +8,7 @@ int main(){/*
     if(age>=18)
     cout<<"you can drive";
     else cout<<"you cannot drive";
+    
     //take positive integer input and tell it is odd or even
     int n;
     cout<<"\nenter anumber:";
@@ -15,6 +16,7 @@ int main(){/*
     if(n%2==0)
     cout<<"this number uis even";
     else cout<<"this number is odd";
+    
     // take positive integer input and tell iof it is divisible by 5 or not
     int nu;
     cout<<"\nenter your number:";
@@ -22,13 +24,14 @@ int main(){/*
     if(nu%5==0)
     cout<<"number is divisible by 5";
     else cout<<"number isn't divisible by 5";
+    
     //take integer input and print the absolute value of that integer
     int m;
      cout<<"\nenter your number:";
      cin>>m;
      if(m<0) cout<<-m;
      else cout<<m;
-     */
+     
      //take real number inputr and check if it is an integer or not
      float i;
      cout<<"\nenter real number:";
@@ -37,6 +40,7 @@ int main(){/*
      if(i==j)
      cout<<"this is integer";
      else cout<< "this is not integer";
+    
      //if cost price and selling price of an item ius input through the keyboard, wap to determine whether the seller 
      // has made profit or incurred loss. also determine how much profit he made or loss he incurred.
      float cp;
@@ -48,6 +52,7 @@ int main(){/*
      if(sp < cp) cout<<"loss="<<cp-sp;
      if(sp > cp)cout<<"profit="<<sp-cp;
      if(sp == cp) cout<<"\nnothing";
+    
      //second method
      float ccp;
      float ssp;
